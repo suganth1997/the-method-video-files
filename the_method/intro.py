eb698143fb001ab15e62bde98201d2453dfdd70f
suganth1997 <suganth.ilangovan@gmail.com>
2022-07-10 22:37:41 +0530

    added buoyancy and book picture

--- the_method/intro.py
+++ the_method/intro.py
@@ -1,9 +1,13 @@
-from math import sqrt
+from math import sqrt, tan
+from tkinter import Image
+from cv2 import circle
 from manim import *
 
 class IntroScene(MovingCameraScene):
     def construct(self):
         archimedes = ImageMobject('Archimedes.jpg').scale(0.25).to_edge(RIGHT)
+
+        archimedes_works = ImageMobject('works_of_archimedes.png').scale(0.5).shift(2*RIGHT)
 
         archimedes_quote_1 = Text("Mathematics reveals its secrets only to those who approach it with pure love,", font_size=20).to_edge(UP).to_edge(LEFT)
         archimedes_quote_2 = Text("for its own beauty.", font_size=20).next_to(archimedes_quote_1, DOWN).to_edge(LEFT)
@@ -43,7 +47,19 @@ class IntroScene(MovingCameraScene):
         cup = VGroup(Line(LEFT+UP, (LEFT+DOWN)), Line((LEFT+DOWN), (RIGHT+DOWN)), Line((RIGHT+DOWN), RIGHT+UP))
 
         water_level = Line(LEFT, RIGHT)
+
+        water_level_ = DashedVMobject(Line(LEFT, RIGHT))
+
+        circle_ball = Circle(0.25, fill_opacity=1).move_to(UP)
+
+        buoyance_law = Text("Law of Buoyancy", font_size=20).to_edge(LEFT)
         
+        eureka = Text("Eureka!", slant=ITALIC, font_size=20).next_to(cup, LEFT)
+
+        geometry_text = Text("Works on Geometry", font_size=20)
+
+        cup_water_setup = VGroup(cup, water_level, water_level_, circle_ball, eureka)        
+
         self.play(Create(fulcrum), Create(center))
 
         # self.play(Create(lever), Create(center), Create(left_circle), Create(right_circle))
@@ -52,12 +68,30 @@ class IntroScene(MovingCameraScene):
 
         self.play(lever_and_all.animate.rotate(PI/12, about_point=ORIGIN))
 
-        self.play(lever_and_all.animate.rotate(-PI/12, about_point=ORIGIN))
+        self.play(lever_and_all.animate.shift(RIGHT + tan(PI/12)*UP))
 
-        self.play(lever_and_all.animate.shift(RIGHT))
+        self.play(lever_and_all.animate.rotate(-PI/12, about_point=ORIGIN))
 
         self.play(Create(lever_law), fulcrum_and_lever.animate.shift((LEFT+UP)*2.25))
 
         self.play(Create(cup))
+
+        self.play(Create(water_level), Create(water_level_), Create(circle_ball))
+
+        self.play(circle_ball.animate.move_to(DOWN*3/4))
+
+        self.play(water_level.animate.shift(UP*0.2))
+
+        self.play(Create(eureka))
+
+        self.play(Create(buoyance_law), cup_water_setup.animate.next_to(buoyance_law, DOWN*1.5 + 0.25*RIGHT))
+
+        self.play(FadeIn(archimedes_works))
+
+        self.play(archimedes_works.animate.to_corner(RIGHT+DOWN))
+
+        geometry_text = geometry_text.next_to(archimedes_works, LEFT).shift(UP)
+
+        self.play(Create(geometry_text))
 
         self.wait(2)
