6da2330bd4fc3fb9673fa4aa8acb3b32709645de
suganth1997 <suganth.ilangovan@gmail.com>
2022-07-13 22:48:10 +0530

    added works on geometry list and fulcrum position adjusted

--- the_method/01_intro.py
+++ the_method/01_intro.py
@@ -5,24 +5,24 @@ class IntroScene(MovingCameraScene):
     def construct(self):
         archimedes = ImageMobject('Archimedes.jpg').scale(0.25).to_edge(RIGHT)
 
-        archimedes_works = ImageMobject('works_of_archimedes.png').scale(0.5).shift(2*RIGHT)
+        archimedes_works = ImageMobject('works_of_archimedes.png').scale(0.5).to_corner(RIGHT+DOWN)
 
         archimedes_quote_1 = Text("Mathematics reveals its secrets only to those who approach it with pure love,", font_size=20).to_edge(UP).to_edge(LEFT)
         archimedes_quote_2 = Text("for its own beauty.", font_size=20).next_to(archimedes_quote_1, DOWN).to_edge(LEFT)
         
         archimedes_name = Text("~ Archimedes", slant=ITALIC, font_size=18).next_to(archimedes_quote_2, DOWN).to_edge(RIGHT)
 
-        self.play(Create(archimedes_quote_1))
+        self.play(Write(archimedes_quote_1))
 
-        self.play(Create(archimedes_quote_2))
+        self.play(Write(archimedes_quote_2))
 
-        self.play(Create(archimedes_name))
+        self.play(Write(archimedes_name))
 
         self.play(FadeIn(archimedes))
 
         self.wait(2)
 
-        self.play(Uncreate(archimedes_quote_1), Uncreate(archimedes_quote_2), Uncreate(archimedes_name))
+        self.play(Unwrite(archimedes_quote_1), Unwrite(archimedes_quote_2), Unwrite(archimedes_name))
 
         self.play(archimedes.animate.to_corner(UP + RIGHT))
 
@@ -38,7 +38,7 @@ class IntroScene(MovingCameraScene):
 
         lever_and_all = VGroup(lever, left_circle, right_circle)
 
-        fulcrum_and_lever = VGroup(fulcrum, center, lever_and_all)
+        fulcrum_and_lever = VGroup(fulcrum, center, lever_and_all).shift(0.2*LEFT)
 
         lever_law = Text("Law of Levers", font_size=20).to_corner(LEFT+UP)
 
@@ -54,7 +54,13 @@ class IntroScene(MovingCameraScene):
         
         eureka = Text("Eureka!", slant=ITALIC, font_size=20).next_to(cup, LEFT)
 
-        geometry_text = Text("Works on Geometry", font_size=20)
+        geometry_text = Text("Works on Geometry", font_size=20).next_to(archimedes_works, LEFT).shift(UP)
+        geometry_text.get_left()
+        geometry_1 = Text("Bounds on Pi", font_size=16).next_to(geometry_text, DOWN)
+
+        geometry_2 = Text("Volume of sphere", font_size=16).next_to(geometry_1, DOWN)
+
+        geometry_3 = Text("...", font_size=16).next_to(geometry_2, DOWN)
 
         cup_water_setup = VGroup(cup, water_level, water_level_, circle_ball, eureka)        
 
@@ -86,10 +92,12 @@ class IntroScene(MovingCameraScene):
 
         self.play(FadeIn(archimedes_works))
 
-        self.play(archimedes_works.animate.to_corner(RIGHT+DOWN))
-
-        geometry_text = geometry_text.next_to(archimedes_works, LEFT).shift(UP)
-
         self.play(Create(geometry_text))
+
+        self.play(Create(geometry_1))
+
+        self.play(Create(geometry_2))
+
+        self.play(Create(geometry_3))
 
         self.wait(2)
