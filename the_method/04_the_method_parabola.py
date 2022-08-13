f063e15219eb47e04aa607c115d3561b0837f3b4
suganth1997 <suganth.ilangovan@gmail.com>
2022-08-13 16:12:19 +0530

    update timings

--- the_method/04_the_method_parabola.py
+++ the_method/04_the_method_parabola.py
@@ -326,8 +326,11 @@ class ParabolaPlot(MovingCameraScene):
 
         parabola_diagram_group_ = VGroup(parabola_graph, line_parabola, tangent_line, left_vertex_axis, AB_line, BC_line)
 
-        self.play(Create(parabola_graph).set_run_time(2))
-        self.play(Create(line_parabola).set_run_time(2))
+        self.play(Create(parabola_graph), run_time=2)
+        self.play(Create(line_parabola), run_time=2)
+
+        self.wait(10)
+
         # self.play(Create(area).set_run_time(4))
 
         # self.wait(4)
@@ -337,19 +340,61 @@ class ParabolaPlot(MovingCameraScene):
         # self.play(a.animate(run_time=4).set_value(2.0))
         # self.play(a.animate(run_time=4).set_value(4.0))
 
+        self.play(Create(A_dot), Create(A_text), Create(C_dot), Create(C_text))
+
         self.play(self.camera.frame.animate.scale(3).move_to([0, 5, 0]))
+
+        self.wait(2)
+
         self.play(Create(tangent_line))
+
+        self.wait(2)
+
         self.play(Create(left_vertex_axis))
+
+        self.wait(4)
+
+        self.play(Create(F_dot), Create(F_text))
+        
+        self.wait(5)
+
+        self.play(Create(D_dot), Create(D_text))
+
         self.play(Create(mid_axis))
-        self.play(Create(any_line_prl_axis))
+
+        self.wait(1)
+        
+        self.play(Create(B_dot), Create(B_text))
+
+        self.wait(1)
+
+        self.play(Create(E_dot), Create(E_text))
+
+        self.wait(8)
+
         self.play(Create(AB_line), Create(BC_line))
+
+        self.wait(4)
+
         self.play(Create(BK_line))
 
-        self.play(Create(A_dot), Create(A_text), Create(C_dot), Create(C_text), Create(B_dot), Create(B_text), Create(D_dot), Create(D_text), Create(E_dot), Create(E_text), Create(F_dot), Create(F_text), Create(O_dot), Create(O_text), Create(M_dot), Create(M_text), Create(P_dot), Create(P_text), Create(N_dot), Create(N_text), Create(K_dot), Create(K_text))
+        self.play(Create(K_dot), Create(K_text))
+
+        self.wait(8)
+
+        self.play(Create(any_line_prl_axis))
+
+        self.play(Create(O_dot), Create(O_text), Create(M_dot), Create(M_text))
+
+        self.play(Create(P_dot), Create(P_text), Create(N_dot), Create(N_text))
 
         self.play(b.animate(run_time=4).set_value(0.75))
 
         self.play(b.animate(run_time=4).set_value(0.25))
+
+        self.wait(2)
+
+        self.wait(6)
 
         # self.wait(4)
 
@@ -357,31 +402,49 @@ class ParabolaPlot(MovingCameraScene):
 
         self.play(Write(quadrature_text))
 
+        self.wait(5)
+
+        self.wait(4)
+
         self.play(FadeIn(MO_line))
+
+        self.wait(4)
         
         self.play(FadeOut(MO_line))
 
+        self.wait(1)
+
         self.play(FadeIn(PO_line))
+
+        self.wait(4)
 
         self.play(FadeOut(PO_line))
 
         self.play(Write(MO_PO_text))
 
         self.play(FadeIn(CA_line))
+
+        self.wait(5)
         
         self.play(FadeOut(CA_line))
 
         self.play(FadeIn(AO_line))
+
+        self.wait(4)
 
         self.play(FadeOut(AO_line))
 
         self.play(Write(CA_AO_text))
 
         self.play(FadeIn(CK_line))
+
+        self.wait(3)
         
         self.play(FadeOut(CK_line))
 
         self.play(FadeIn(KN_line))
+
+        self.wait(4)
 
         self.play(FadeOut(KN_line))
 
@@ -389,15 +452,25 @@ class ParabolaPlot(MovingCameraScene):
 
         self.play(Write(CK_KN_text))
 
+        self.wait(4)
+
         self.play(Unwrite(quadrature_text))
         
         self.play(Write(the_method_text))
 
         self.play(ratio_relation.animate.shift(4*DOWN))
 
-        self.play(Create(HK_line), Create(H_dot), Create(H_text))
+        self.wait(1)
+
+        self.play(Create(HK_line))
+
+        self.play(Create(H_dot), Create(H_text))
+
+        self.wait(4)
 
         self.play(FadeIn(HK_line_))
+
+        self.wait(2)
 
         self.play(FadeOut(HK_line_))
 
@@ -405,11 +478,13 @@ class ParabolaPlot(MovingCameraScene):
 
         self.play(Write(HK_KN_text))
 
+        self.wait(4)
+
         self.play(Unwrite(MO_PO_text), Unwrite(HK_KN_text))
 
         self.play(Write(fulcrum_lever_text))
 
-        self.wait(2)
+        self.wait(4)
 
         # TODO: animate fulcrum and lever with line lengths, and conclude with archimedes quote
         
@@ -429,6 +504,8 @@ class ParabolaPlot(MovingCameraScene):
 
         self.play(b.animate(run_time=2).set_value(0.25))
 
+        self.wait(4)
+
         self.play(fulcrum_lever_text.animate.shift(8*UP + 7*LEFT))
 
         self.play(Create(lever_all), self.camera.frame.animate.move_to([-15, 6, 0]), fulcrum_lever_text.animate.shift(7*LEFT), the_method_text.animate.shift(7*LEFT))
@@ -447,6 +524,8 @@ class ParabolaPlot(MovingCameraScene):
 
         self.play(ReplacementTransform(MO_line_, MO_line_f))
 
+        self.wait(5)
+
         self.play(b.animate(run_time=6).set_value(0.75))
 
         self.play(b.animate(run_time=2).set_value(1.0))
@@ -462,8 +541,12 @@ class ParabolaPlot(MovingCameraScene):
         # self.play(area.animate.scale(1.0/1.75), run_time=2)
 
         self.play(area.animate.move_to(lever_left_coord), run_time=2)
+        
+        self.wait(2)
 
         self.play(Create(area_triangle))
+
+        
 
         # self.play(area_triangle.animate.scale(1.0/1.75), run_time=2)
 
@@ -482,6 +565,8 @@ class ParabolaPlot(MovingCameraScene):
 
         self.play(b.animate(run_time=2).set_value(0.0))
 
+        self.wait(6)
+
         lever_left_coord, lever_right_coord = get_left_and_right_coord()
 
         MO_line_f_ = Line(lever_right_coord - AF_length*UP/2, lever_right_coord + AF_length*UP/2)
@@ -495,10 +580,6 @@ class ParabolaPlot(MovingCameraScene):
 
         MO_line_f_.add_updater(MO_line_f__updater)
 
-        self.play(ReplacementTransform(area_triangle, triangle_vis_eq), ReplacementTransform(MO_line_f, MO_line_f_))
-
-        self.play(b.animate(run_time=2).set_value(1.0))
-
         self.play(FadeIn(KN_line))
 
         self.play(FadeOut(KN_line))
@@ -506,6 +587,10 @@ class ParabolaPlot(MovingCameraScene):
         self.play(FadeIn(HK_line_))
 
         self.play(FadeOut(HK_line_))
+
+        self.play(ReplacementTransform(area_triangle, triangle_vis_eq), ReplacementTransform(MO_line_f, MO_line_f_))
+
+        self.play(b.animate(run_time=2).set_value(1.0))        
 
         for mobj in parabola_diagram_group:
             try:
@@ -526,9 +611,15 @@ class ParabolaPlot(MovingCameraScene):
 
         HK_KN_text = MathTex("KN = \\frac{HK}{3}", font_size = 58).next_to(lever_right_coord, 5*UP)
 
+        self.wait(5)
+
         self.play(FadeIn(temp_line_to_centroid))
 
+        self.wait(10)
+
         self.play(Write(HK_KN_text), FadeIn(temp_line_to_center))
+
+        self.wait(4)
 
         self.play(FadeOut(temp_line_to_centroid), FadeOut(temp_line_to_center))
 
@@ -539,6 +630,8 @@ class ParabolaPlot(MovingCameraScene):
         area_c = area.get_center()
 
         self.play(area.animate.shift(area_c - area_up), triangle_vis_eq.animate.shift(KC_length*DOWN*2.0/3.0), self.camera.frame.animate.move_to([-20, 0, 0]), fulcrum_lever_text.animate.shift(6*DOWN), the_method_text.animate.shift(6*DOWN))
+
+        self.wait(2)
 
         area_fulcrum_text = MathTex("\\text{Area of Parabola} \\times HK = \\text{Area of Triangle} \\times KN", font_size = 58).move_to(fulcrum_lever_text.get_left()).shift(5*RIGHT)
 
@@ -554,15 +647,23 @@ class ParabolaPlot(MovingCameraScene):
 
         self.play(Write(area_fulcrum_text))
 
+        self.wait(2)
+
         self.add(HK_KN_text_)
 
         self.play(ReplacementTransform(HK_KN_text, area_fulcrum_text))
 
+        self.wait(2)
+
         self.play(ReplacementTransform(area_fulcrum_text, area_fulcrum_text_final))
+
+        self.wait(2)
 
         self.play(parabola_diagram_group_.animate.scale(0.5))
 
         self.play(parabola_diagram_group_.animate.shift(22.5*LEFT))
+
+        self.wait(2)
 
         area_ = axes.get_area(parabola_graph, [-sqrt(1.0/a.get_value()), sqrt(1.0/a.get_value())], bounded=line_parabola, dx_scaling=10).scale(0.5).move_to(parabola_graph.get_center())
 
@@ -570,11 +671,17 @@ class ParabolaPlot(MovingCameraScene):
 
         self.play(FadeIn(area_))
 
+        self.wait(2)
+
         self.play(FadeOut(area_))
 
         self.play(FadeIn(area_triangle_))
 
+        self.wait(2)
+
         self.play(FadeOut(area_triangle_))
+
+        self.wait(6)
 
         self.play(Write(area_inscribed_triangle))
 
